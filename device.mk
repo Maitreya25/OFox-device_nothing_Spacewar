f3d11760cfc329278b9275188bb2ba54be809c2a
Maitreya25 <maitreyapatni25@gmail.com>
2023-12-01 15:12:05 +0000

    Load touch modules for stock

--- device.mk
+++ device.mk
@@ -96,7 +96,7 @@ TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 
-TW_LOAD_VENDOR_MODULES := "goodix_fp.ko adsp_loader_dlkm.ko modules.load msm_drm.ko q6_notifier_dlkm.ko q6_pdr_dlkm.ko sensors_ssc.ko qti_battery_charger_main.ko"
+TW_LOAD_VENDOR_MODULES := "goodix_fp.ko adsp_loader_dlkm.ko modules.load msm_drm.ko q6_notifier_dlkm.ko q6_pdr_dlkm.ko sensors_ssc.ko qti_battery_charger_main.ko fts_tp.ko"
 
 # Haptics fix
 TW_SUPPORT_INPUT_AIDL_HAPTICS_FIX_OFF := true
